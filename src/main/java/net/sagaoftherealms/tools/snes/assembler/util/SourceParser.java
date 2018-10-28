5bca1320f6d6b8912bd5c8b9447d4590d263c7d1
Summers Pittman <secondsun@gmail.com>
2018-10-28 09:35:21 -0400

    Parser broken tests

---
+++ src/main/java/net/sagaoftherealms/tools/snes/assembler/util/SourceParser.java
@@ -0,0 +1,9 @@
+package net.sagaoftherealms.tools.snes.assembler.util;
+
+public class SourceParser {
+    private final SourceScanner scanner;
+
+    public SourceParser(SourceScanner scanner) {
+        this.scanner = scanner;
+    }
+}
---
+++ src/test/resources/parseTest/basicenum.s
@@ -0,0 +1,4 @@
+;Line 1
+.ENUM $C000
+.ENDE
+;--------Line 4
